244c26a812384825f1331cedc299ab3b54748443
team-virtura-tech <team.virturatech@gmail.com>
2025-11-16 02:25:38 -0800

    [PATCH] Contact updates (#13)
    * [FEATURE] Add new UI components and styles: Globe, GradualSpacing, Spinner, Toast, and WaveBackground
    
    * [FEATURE] Enhance Contact Us page with form validation and toast notifications
    
    ---------
    
    Co-authored-by: jainil04 <33036617+jainil04@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self'; frame-src 'self' https://www.google.com https://www.gstatic.com https://recaptcha.google.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
